a0794cc2f6317a71e6a424f19613046c4585b1a3
Yash Raj <yashraj941015@gmail.com>
2024-01-03 23:41:27 +0530

    making react element using React.createElement

--- basicproject/src/main.jsx
+++ basicproject/src/main.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// we can create our own element in the form i which the react bnundler convert a html syntax
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com', target:"_blank"},
+  'Visit Google here'
+  )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  reactElement       // in this way we can also render a element and also any function that returns Html 
 )
