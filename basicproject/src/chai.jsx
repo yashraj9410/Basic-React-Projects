e453f9bfb8c638055b90a3e00c54605d033fd9b1
Yash Raj <yashraj941015@gmail.com>
2024-01-04 21:26:51 +0530

    internal object component: React.createElement

--- basicproject/src/chai.jsx
+++ basicproject/src/chai.jsx
@@ -1,11 +1,12 @@
+const name = "Yash Raj"  // we can use the name inside the return component 
+                         // NOTE : we can only pass a evaluated Expression with final value inside the compinent in the tag {}
+
 function Chai() {
     return (
         <p>
-            <h1>Hello how are you doing</h1>
+            <h1>Hello how are you doing {name}</h1>
         </p>
     )
 }
 // component name should have a upper case starting 
 export default Chai
-
-// started again with the basics 
