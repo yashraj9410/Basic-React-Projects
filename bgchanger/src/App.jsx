c35d3793a59a788863b09d7482bf2ccfca2bbb25
Yash Raj <yashraj941015@gmail.com>
2024-01-06 23:07:56 +0530

    added:bgChanger

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const buttons = ['red','green','blue','black','yellow','pink']
 
   return (
     <>
-      
+      <div ></div>
     </>
   )
 }
---
+++ bgchanger/src/components/ButtonComp.jsx
@@ -0,0 +1,9 @@
+// this is the button component
+
+function ButtonComp(props) {
+  return (
+    <button></button>
+  )
+}
+
+export default ButtonComp
