96edb74d8ef637c97a8c200a490caa630ab6126a
Yash Raj <yashraj941015@gmail.com>
2024-01-06 22:10:42 +0530

    updated: added props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -4,16 +4,25 @@ import Card from './components/Card'
 function App() {
 
   const [count, setCount] = useState(0)
-  let obj = {
+
+  let garhwal_obj = {
     title:"The Warm Sunset",
     place:"Scenic beauty of garhwal himalyas",
     tags:"#Uttarakhand"
   }
 
+  let kumaon_obj = {
+    title:"The Cold Sunset",
+    place:"Scenic beauty of kumaon himalyas",
+    tags:"#Uttarakhand"
+  }
+
+
+
   return (
     <>
-    <Card />
-    <Card myObj ={obj}/>
+    <Card title= {kumaon_obj.title} place={kumaon_obj.place} tags = {kumaon_obj.tags}/>
+    <Card title= {garhwal_obj.title} place={garhwal_obj.place} tags = {garhwal_obj.tags}/>
     </>
   )
 }
