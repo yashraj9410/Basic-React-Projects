5975675ecc2431463cf2e1ca505376d2f5fb0019
Yash Raj <yashraj941015@gmail.com>
2024-01-07 23:10:06 +0530

    passwordGenerator

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // maintaing the hook for all the factors that affect the password
+  const [length, setLength] = useState(8);
+  const [numberAllow, setNumberAllow] = useState(false);
+  const [symbolAllow, setSymbolAllow] = useState(false);
 
   return (
     <>
-      
+      <div></div>
     </>
   )
 }
