90fd85927e18d1d3a76a02b81d6c6a6b289fd75e
Yash Raj <yashraj941015@gmail.com>
2024-01-07 23:31:52 +0530

    added useCallback hook for password generation

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 function App() {
 
@@ -6,6 +6,23 @@ function App() {
   const [length, setLength] = useState(8);
   const [numberAllow, setNumberAllow] = useState(false);
   const [symbolAllow, setSymbolAllow] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // creating the useCallback hook for the password genration it takes a call back and a dependency list
+  // the useCallback hook caches a function definition between multiple re renders ie the function will always run if any of the dependency is altered without a reload 
+  const createPassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllow) str = str + "1234567890"
+    if(symbolAllow) str = str + "!@#$%^&*"
+
+    for(let i = 1 ; i<=length;i++){
+      let index =  Math.floor(Math.random()*str.length +1)
+      pass = pass + str.charAt(index);
+    }
+    
+  }
+    , [length, numberAllow, symbolAllow, setPassword])  // we are passing sePassword that is because that only sets the state of password
 
   return (
     <>
