246ab9b51872ad5b996935240cc58f2535d33a45
Yash Raj <yashraj941015@gmail.com>
2024-01-16 21:49:54 +0530

    User Context and Provider setup

--- contextapibasic/src/App.jsx
+++ contextapibasic/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <UserContextProvider>
+        
+      </UserContextProvider>
     </>
   )
 }
--- contextapibasic/src/context/UserContextProvider.jsx
+++ contextapibasic/src/context/UserContextProvider.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import UserContext from './userContext'
 
-function UserContextProvider() {
+function UserContextProvider({children}) {
+    const [user,setuser] = React.useState(null);
+    // fetch all data here and then throw in value 
   return (
-    <div>UserContextProvider</div>
+    <UserContext.Provider value={{user,setuser}}>
+    </UserContext.Provider>
   )
 }
 
