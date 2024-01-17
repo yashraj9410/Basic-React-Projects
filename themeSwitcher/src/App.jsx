b0903b60b04546d66bb670eb37110d20076713df
Yash Raj <yashraj941015@gmail.com>
2024-01-17 22:02:49 +0530

    added use Context and context  provider

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react'
 import { ThemeProvider } from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
 
   const [themeMode,setThememode] = useState("light");
 
-  const light = ()=>{
+  const lightTheme = ()=>{
     setThememode("light")
   }
 
-  const dark = () =>{
+  const darkTheme = () =>{
     setThememode("dark")
   }
 
@@ -25,11 +27,11 @@ function App() {
         <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
+                        <ThemeBtn />
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       
+                       <Card />
                     </div>
                 </div>
         </div>
