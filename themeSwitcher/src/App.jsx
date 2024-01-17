61edab77b8a66eb79920b442d59df3b70b2e2308
Yash Raj <yashraj941015@gmail.com>
2024-01-17 21:53:04 +0530

    added use Contect and provider

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,11 +1,40 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { ThemeProvider } from './contexts/Theme'
 
 function App() {
 
+  const [themeMode,setThememode] = useState("light");
+
+  const light = ()=>{
+    setThememode("light")
+  }
+
+  const dark = () =>{
+    setThememode("dark")
+  }
+
+  // actual chnage in theme will be caused by this use effect hook
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("dark","light")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
   return (
     <>
-      
+      <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+        </div>
+      </ThemeProvider>
+
     </>
   )
 }
---
+++ themeSwitcher/src/contexts/Theme.js
@@ -0,0 +1,15 @@
+import React, {useContext, createContext} from "react";{}
+
+const ThemeContext = createContext(
+    {
+        themeMode:"light",
+        lightTheme:() => {},
+        darkTheme:() => {}
+    }
+);
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme(){
+    return useContext(ThemeContext);
+}
