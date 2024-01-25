9376c3b2a8f90ba597422c1643d119f1fd5f1703
Yash Raj <yashraj941015@gmail.com>
2024-01-25 23:00:12 +0530

    added todoSlice , functionalites

--- reduxToolkit/src/features/todo/todoSlice.js
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -1,5 +1,7 @@
 import {createSlice , nanoid} from '@reduxjs/toolkit'
 
+
+// this is going to be used as state 
 const initialState = {
     todos:[
         {
@@ -9,15 +11,30 @@ const initialState = {
     ]
 }
 
-const todoSlice  = createSlice({
+// state, action -> state is the initial state and action is used to make changes to current state
+// the property name , reducers , initialState are defined and need to be same as key
+// reducers are used to make changes using functions 
+// payload is also an object and we can add properties 
+
+export const todoSlice  = createSlice({
     name:'todo',
     initialState,
     reducers: {
         addTodo:(state, action) => {
-
+            const todo = {
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
         },
         removeTodo: (state,action)=> {
-            
-        }
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
     }
 })
+
+// we have to export the individual functionalities
+export const {addTodo,removeTodo} = todoSlice.actions
+
+// export the list of all reducers to the store.
+export default todoSlice.reducer
