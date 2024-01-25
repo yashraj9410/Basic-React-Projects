4665f6d718608ff71c3b9783111ae073ba3a4f1e
Yash Raj <yashraj941015@gmail.com>
2024-01-25 23:28:16 +0530

    added useSelector in Todo

--- reduxToolkit/src/components/Todo.jsx
+++ reduxToolkit/src/components/Todo.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
+
+
+// useSelector is used to access hook from the store state 
 
 function Todo() {
+
+    // we get the access of state 
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+
   return (
-    <div>Todo</div>
+    <>
+    <div>Todos</div>
+    {todos.map((todo)=>(
+        <li key={todo.id}>{todo.text}
+        <button onClick={()=> dispatch(removeTodo(todo.id))}></button>
+        </li>
+    ))}
+    </>
+    
   )
 }
 
