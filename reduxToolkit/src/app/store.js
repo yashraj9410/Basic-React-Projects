f0adea5d5b5d91592fd852828d9808c0258d6c82
Yash Raj <yashraj941015@gmail.com>
2024-01-25 23:04:12 +0530

    added todoSlice , functionalites

--- reduxToolkit/src/app/store.js
+++ reduxToolkit/src/app/store.js
@@ -1,3 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
+import todoReducer from '../features/todo/todoSlice' 
 
-export const store  = configureStore({});
+// we have to use all the rducers here from todoSlice
+export const store  = configureStore({
+    reducer: todoReducer
+});
